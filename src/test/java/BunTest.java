265922b0b499da3240b55e119f7f3e2e7e77a87e
Alex <forhousemd@mail.ru>
2022-05-03 19:23:18 +0300

    Подключены библиотеки: Jacoco, Mockito, JUnit 4 (изменен файл pom.xml). Написаны тесты на класс Bun Написаны тесты на класс Ingredient и IngredientType Обновлены тесты на класс Bun

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -5,7 +5,7 @@ import praktikum.Bun;
 
 public class BunTest {
 
-    private Bun bun;
+    private Bun bunForTest;
     private String bunNameForTest = "name";
     private float bunPriceForTest = (float) 12.12345;
 
@@ -13,21 +13,21 @@ public class BunTest {
     //Создаются объекты для тестов
     @Before
     public void createBunObjectsBeforeTest() {
-        bun = new Bun(bunNameForTest, bunPriceForTest);
+        bunForTest = new Bun(bunNameForTest, bunPriceForTest);
     }
 
     //Проверяется метод getName класса Bun
     @Test
     public void testBunGetName() {
         createBunObjectsBeforeTest();
-        Assert.assertEquals(bunNameForTest, bun.getName());
+        Assert.assertEquals(bunNameForTest, bunForTest.getName());
     }
 
     //Проверяется метод getPrice класса Bun
     @Test
     public void testBunGetPrice() {
         createBunObjectsBeforeTest();
-        Assert.assertEquals(bunPriceForTest, bun.getPrice(), 0.0f);
+        Assert.assertEquals(bunPriceForTest, bunForTest.getPrice(), 0.0f);
     }
 
 }
